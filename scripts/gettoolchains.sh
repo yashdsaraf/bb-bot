8d42a66693f95c23f5d772eae300841a6c8a16d1
yashdsaraf <yashdsaraf@gmail.com>
2017-02-06 01:13:15 +0530

    Fix typo in script

--- scripts/gettoolchains.sh
+++ scripts/gettoolchains.sh
@@ -3,7 +3,7 @@ CURRDIR=$PWD
 cd "`dirname $0`"
 URL="https://www.dropbox.com/sh/sovqvaf2p86og06/AAAyRYCRXHWfCy2uabMB34Qfa?dl=1"
 wget $URL -O toolchains.zip || exit 1
-unzip -o toolchains.zip '*.tar.xz' -d toolchains
+unzip -o toolchains.zip '*.tar.xz' toolchains.md5 -d toolchains
 cd toolchains
 md5sum -c toolchains.md5 || exit 1
 for i in *.tar.xz
