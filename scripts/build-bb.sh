2194b951a59dff2497c50bed794dc0d6cc004efd
yashdsaraf <yashdsaraf@gmail.com>
2017-02-06 22:53:42 +0530

    Add GPLv3 license to project [ci skip]

--- scripts/build-bb.sh
+++ scripts/build-bb.sh
@@ -1,4 +1,20 @@
 #!/usr/bin/env bash
+# Copyright 2016 Yash D. Saraf
+# This file is part of BB-Bot.
+
+# BB-Bot is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# BB-Bot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with BB-Bot.  If not, see <http://www.gnu.org/licenses/>.
+
 . ./toolchain-exports.sh
 ##Build busybox auto-magically
 CURRDIR=$PWD
@@ -33,10 +49,10 @@ build() {
 }
 
 make mrproper
-echo -e "\nBuilding Non-SELinux busybox--\n\n"
+echo -e "\nBuilding Non-SELinux busybox--\n"
 cp conf_no_selinux .config
 build $*
-echo -e "\nBuilding SELinux busybox--\n\n"
+echo -e "\nBuilding SELinux busybox--\n"
 current=sel
 cp conf_selinux .config
 build $*
